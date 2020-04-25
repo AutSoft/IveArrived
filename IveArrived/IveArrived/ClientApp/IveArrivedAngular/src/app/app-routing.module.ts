7770f010804ed288f6dcf1cece86b0750a767f04
Zsolt Berényi <berenyi.zsolt@autsoft.hu>
2020-04-25 14:00:06 +0200

    home-init

--- IveArrived/IveArrived/ClientApp/IveArrivedAngular/src/app/app-routing.module.ts
+++ IveArrived/IveArrived/ClientApp/IveArrivedAngular/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full'  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
