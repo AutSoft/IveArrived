4995559d5ac655a06d8287e3e22a7ccd1c2c7c73
kovacsa97 <adamkovacs.liverpool@gmail.com>
2020-04-25 13:59:38 +0200

    material

--- IveArrived/IveArrived/ClientApp/IveArrivedAngular/src/app/app.module.ts
+++ IveArrived/IveArrived/ClientApp/IveArrivedAngular/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/register/register.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
