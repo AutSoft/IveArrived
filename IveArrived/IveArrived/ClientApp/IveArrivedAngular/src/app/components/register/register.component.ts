c0b26e3a2f1c83434d02c2d9b6736fc8d002d7c2
kovacsa97 <adamkovacs.liverpool@gmail.com>
2020-04-25 21:17:24 +0200

    frontend

--- IveArrived/IveArrived/ClientApp/IveArrivedAngular/src/app/components/register/register.component.ts
+++ IveArrived/IveArrived/ClientApp/IveArrivedAngular/src/app/components/register/register.component.ts
@@ -1,17 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import { AccountService } from 'src/app/api/app.generated';
+import { AccountService, RegistrationModel } from 'src/app/api/app.generated';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
+  providers: [AccountService]
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private accountService: AccountService) { }
+
+  files: any[];
+
+  smeName: string;
+  email: string;
+  phoneNumber:string;
+  profile: string;
+  contactName:string;
+  address: string;
+  city: string;
+  zipcode: string;
 
   ngOnInit(): void {
+  }
 
+  fileChanged(file: any[]) {
+    this.files = file;
+    console.log(file);
+  }
+
+  onSubmit(){
+    let model = new RegistrationModel();
+    model.email = this.email;
+    model.role = this.profile;
+    model.userName = this.smeName;
   }
 
 }
