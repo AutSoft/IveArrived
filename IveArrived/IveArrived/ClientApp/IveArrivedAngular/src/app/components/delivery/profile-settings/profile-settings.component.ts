8ee11bcedf46c264bcced744febcf1d0f704a5d5
Zsolt Berényi <berenyi.zsolt@autsoft.hu>
2020-04-26 14:17:35 +0200

    edit account

--- IveArrived/IveArrived/ClientApp/IveArrivedAngular/src/app/components/delivery/profile-settings/profile-settings.component.ts
+++ IveArrived/IveArrived/ClientApp/IveArrivedAngular/src/app/components/delivery/profile-settings/profile-settings.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService, FileParameter } from 'src/app/api/app.generated';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile-settings',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileSettingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private accountService : AccountService, private router : Router) { }
+
+  files: FileParameter;
+
+  smeName: string = "";
+  phoneNumber: string = "";
+  profile: string  = "";
+  contactName: string = "";
+  address: string = "";
+  city: string ="";
+  zipcode: string ="";
 
   ngOnInit(): void {
+  }
+
+  fileChanged(file: FileParameter[]) {
+    this.files = file[0];
+  }
+
+  onSubmit() {
+    this.accountService.updateSME(this.smeName, this.contactName, this.address, 
+      this.city, this.zipcode, this.phoneNumber,null, this.files).subscribe();
   }
 
 }
